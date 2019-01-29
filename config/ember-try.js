83958cf0e21342ebaec7f1c906d1b2a47cdd6c53
Jan Buschtöns <jan@buschtoens.me>
2019-01-29 21:16:22 +0100

    chore(ci): test Ember 2.13

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,6 +12,20 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
+          name: 'ember-2.13',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': true
+            })
+          },
+          npm: {
+            devDependencies: {
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~2.13.0'
+            }
+          }
+        },
+        {
           name: 'ember-lts-2.18',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
