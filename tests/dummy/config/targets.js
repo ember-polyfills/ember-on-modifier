96c8277b3453c40ed79949035396cb55747bb755
Jan Buschtöns <buschtoens@gmail.com>
2019-03-06 10:25:41 +0100

    chore(ci/appveyor): setup AppVeyor with IE11 (#13)

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -6,10 +6,11 @@ const browsers = [
   'last 1 Safari versions'
 ];
 
+const isIE = !!process.env.IE;
 const isCI = !!process.env.CI;
 const isProduction = process.env.EMBER_ENV === 'production';
 
-if (isCI || isProduction) {
+if (isIE || isCI || isProduction) {
   browsers.push('ie 11');
 }
 
