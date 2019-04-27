6ab0e3a59fc221b343a1e542058fa800a23248ec
Jan Buschtöns <jan@buschtoens.me>
2019-04-27 10:52:40 +0200

    test: ensure stability (#49)
    * test: add `assert.counts` helper
    
    * test: unrelated prop updates & `once`
    
    * test: assert against unnecessary re-registers
    
    * test: wrap `set` in run loop
    
    * test: use proper `this` for `assert.counts`
    
    * test: fix `assert.count` helper

--- tests/integration/modifiers/on-test.js
+++ tests/integration/modifiers/on-test.js
@@ -17,7 +17,7 @@ module('Integration | Modifier | on', function(hooks) {
   hooks.afterEach(() => resetOnerror());
 
   test('it basically works', async function(assert) {
-    assert.expect(4);
+    assert.expect(6);
 
     this.someMethod = function(event) {
       assert.ok(
@@ -40,11 +40,15 @@ module('Integration | Modifier | on', function(hooks) {
       hbs`<button data-foo="some-thing" {{on "click" this.someMethod}}></button>`
     );
 
+    assert.counts({ adds: 1, removes: 0 });
+
     await click('button');
+
+    assert.counts({ adds: 1, removes: 0 });
   });
 
   test('it can accept the `once` option', async function(assert) {
-    assert.expect(1);
+    assert.expect(3);
 
     let n = 0;
     this.someMethod = () => n++;
@@ -53,18 +57,65 @@ module('Integration | Modifier | on', function(hooks) {
       hbs`<button {{on "click" this.someMethod once=true}}></button>`
     );
 
+    assert.counts({ adds: 1, removes: 0 });
+
     await click('button');
     await click('button');
+
+    assert.counts({ adds: 1, removes: 0 });
 
     assert.strictEqual(n, 1, 'callback has only been called once');
   });
 
-  test('it can accept the `capture` option', async function(assert) {
-    assert.expect(1);
+  test('unrelated property changes do not break the `once` option', async function(assert) {
+    assert.expect(5);
 
-    const calls = [];
-    this.outerListener = () => calls.push('outer');
-    this.innerListener = () => calls.push('inner');
+    let n = 0;
+    this.someMethod = () => n++;
+    this.someProp = 0;
+
+    await render(
+      hbs`<button {{on "click" this.someMethod once=true}}>{{this.someProp}}</button>`
+    );
+
+    assert.counts({ adds: 1, removes: 0 });
+
+    await click('button');
+    await click('button');
+
+    assert.counts({ adds: 1, removes: 0 });
+
+    assert.strictEqual(n, 1, 'callback has only been called once');
+
+    run(() => set(this, 'someProp', 1));
+    await settled();
+    assert.counts({ adds: 1, removes: 0 });
+
+    await click('button');
+    assert.strictEqual(n, 1, 'callback has only been called once');
+  });
+
+  test('unrelated property changes do not cause the listener to re-register', async function(assert) {
+    assert.expect(2);
+
+    this.someMethod = () => {};
+    this.someProp = 0;
+
+    await render(
+      hbs`<button {{on "click" this.someMethod}}>{{this.someProp}}</button>`
+    );
+    assert.counts({ adds: 1, removes: 0 });
+
+    run(() => set(this, 'someProp', 1));
+    await settled();
+    assert.counts({ adds: 1, removes: 0 });
+  });
+
+  test('it can accept the `capture` option', async function(assert) {
+    assert.expect(5);
+
+    this.outerListener = () => assert.step('outer');
+    this.innerListener = () => assert.step('inner');
 
     await render(hbs`
       <div {{on "click" this.outerListener capture=true}}>
@@ -72,21 +123,23 @@ module('Integration | Modifier | on', function(hooks) {
       </div>
     `);
 
+    assert.counts({ adds: 2, removes: 0 });
+
     await click('button');
 
-    assert.deepEqual(
-      calls,
+    assert.counts({ adds: 2, removes: 0 });
+
+    assert.verifySteps(
       ['outer', 'inner'],
       'outer capture listener was called first'
     );
   });
 
   test('it can accept the `once` & `capture` option combined', async function(assert) {
-    assert.expect(1);
+    assert.expect(6);
 
-    const calls = [];
-    this.outerListener = () => calls.push('outer');
-    this.innerListener = () => calls.push('inner');
+    this.outerListener = () => assert.step('outer');
+    this.innerListener = () => assert.step('inner');
 
     await render(hbs`
       <div {{on "click" this.outerListener once=true capture=true}}>
@@ -94,18 +147,21 @@ module('Integration | Modifier | on', function(hooks) {
       </div>
     `);
 
+    assert.counts({ adds: 2, removes: 0 });
+
     await click('button');
     await click('button');
 
-    assert.deepEqual(
-      calls,
+    assert.counts({ adds: 2, removes: 0 });
+
+    assert.verifySteps(
       ['outer', 'inner', 'inner'],
       'outer capture listener was called first and was then unregistered'
     );
   });
 
   test('it raises an assertion when calling `event.preventDefault()` on a `passive` event', async function(assert) {
-    assert.expect(1);
+    assert.expect(3);
 
     this.handler = event => {
       assert.expectAssertion(
@@ -118,13 +174,17 @@ module('Integration | Modifier | on', function(hooks) {
       hbs`<button {{on "click" this.handler passive=true}}></button>`
     );
 
+    assert.counts({ adds: 1, removes: 0 });
+
     await click('button');
+
+    assert.counts({ adds: 1, removes: 0 });
   });
 
   (gte('3.0.0') // I have no clue how to catch the error in Ember 2.13
     ? test
     : skip)('it raises an assertion if an invalid event option is passed in', async function(assert) {
-    assert.expect(1);
+    assert.expect(2);
 
     setupOnerror(function(error) {
       assert.strictEqual(
@@ -137,13 +197,14 @@ module('Integration | Modifier | on', function(hooks) {
     await render(
       hbs`<button {{on "click" this.someMethod nope=true foo=false}}></button>`
     );
+
+    assert.counts({ adds: 0, removes: 0 });
   });
 
   (gte('3.0.0') // I have no clue how to catch the error in Ember 2.13
     ? test
     : skip)('it raises an assertion if an invalid event name or callback is passed in', async function(assert) {
-    const errors = [];
-    setupOnerror(error => errors.push(error));
+    setupOnerror(error => assert.step(error.message));
 
     await render(hbs`<button {{on "click" 10}}></button>`);
     await render(hbs`<button {{on "click"}}></button>`);
@@ -151,7 +212,9 @@ module('Integration | Modifier | on', function(hooks) {
     await render(hbs`<button {{on 10 undefined}}></button>`);
     await render(hbs`<button {{on}}></button>`);
 
-    assert.deepEqual(errors.map(e => e.message), [
+    assert.counts({ adds: 0, removes: 0 });
+
+    assert.verifySteps([
       "Assertion Failed: ember-on-modifier: '10' is not a valid callback. Provide a function.",
       "Assertion Failed: ember-on-modifier: 'undefined' is not a valid callback. Provide a function.",
       "Assertion Failed: ember-on-modifier: '' is not a valid event name. It has to be a string with a minimum length of 1 character.",
@@ -163,7 +226,7 @@ module('Integration | Modifier | on', function(hooks) {
   (gte('3.0.0') // I have no clue how to catch the error in Ember 2.13
     ? test
     : skip)('it recovers after updating to incorrect parameters', async function(assert) {
-    assert.expect(3);
+    assert.expect(9);
 
     const errors = [];
     setupOnerror(error => errors.push(error));
@@ -174,77 +237,88 @@ module('Integration | Modifier | on', function(hooks) {
     await render(
       hbs`<button data-foo="some-thing" {{on "click" this.someMethod}}></button>`
     );
+    assert.counts({ adds: 1, removes: 0 });
 
     await click('button');
     assert.strictEqual(n, 1);
+    assert.counts({ adds: 1, removes: 0 });
 
     run(() => set(this, 'someMethod', undefined));
     await settled();
+    assert.counts({ adds: 1, removes: 1 });
 
     await click('button');
     assert.strictEqual(n, 1);
+    assert.counts({ adds: 1, removes: 1 });
 
     run(() => set(this, 'someMethod', () => n++));
     await settled();
+    assert.counts({ adds: 2, removes: 2 });
 
     await click('button');
     assert.strictEqual(n, 2);
+    assert.counts({ adds: 2, removes: 2 });
   });
 
-  test('it passes additional parameters though to the listener', async function(assert) {
-    assert.expect(4);
-
-    const calls = [];
+  test('it passes additional parameters through to the listener', async function(assert) {
+    assert.expect(11);
 
     this.a = 1;
     this.b = 3;
     this.c = 5;
     this.someMethod = (a, b, c, event) => {
-      calls.push([a, b, c]);
+      assert.step([a, b, c].join('-'));
       assert.ok(event instanceof MouseEvent, 'last parameter is an event');
     };
 
     await render(
       hbs`<button {{on "click" this.someMethod this.a this.b this.c}}>{{this.c}}</button>`
     );
+    assert.counts({ adds: 1, removes: 0 });
 
     await click('button');
     await click('button');
+    assert.counts({ adds: 1, removes: 0 });
 
     run(() => set(this, 'c', 7));
     await settled();
+    assert.counts({ adds: 2, removes: 1 });
 
     await click('button');
+    assert.counts({ adds: 2, removes: 1 });
 
-    assert.deepEqual(
-      calls,
-      [[1, 3, 5], [1, 3, 5], [1, 3, 7]],
+    assert.verifySteps(
+      [[1, 3, 5], [1, 3, 5], [1, 3, 7]].map(s => s.join('-')),
       'parameters were passed through and updated on change'
     );
   });
 
   test('it is re-registered, when the callback changes', async function(assert) {
-    assert.expect(2);
+    assert.expect(6);
 
     let a = 0;
     this.someMethod = () => a++;
 
     await render(hbs`<button {{on "click" this.someMethod}}></button>`);
+    assert.counts({ adds: 1, removes: 0 });
 
     await click('button');
+    assert.counts({ adds: 1, removes: 0 });
 
     let b = 0;
     run(() => set(this, 'someMethod', () => b++));
     await settled();
+    assert.counts({ adds: 2, removes: 1 });
 
     await click('button');
+    assert.counts({ adds: 2, removes: 1 });
 
     assert.strictEqual(a, 1);
     assert.strictEqual(b, 1);
   });
 
   test('it is re-registered, when the callback changes and `capture` is used', async function(assert) {
-    assert.expect(3);
+    assert.expect(9);
 
     let a = 0;
     this.someMethod = () => a++;
@@ -253,14 +327,18 @@ module('Integration | Modifier | on', function(hooks) {
     await render(
       hbs`<button {{on "click" this.someMethod capture=this.capture}}></button>`
     );
+    assert.counts({ adds: 1, removes: 0 });
 
     await click('button');
+    assert.counts({ adds: 1, removes: 0 });
 
     let b = 0;
     run(() => set(this, 'someMethod', () => b++));
     await settled();
+    assert.counts({ adds: 2, removes: 1 });
 
     await click('button');
+    assert.counts({ adds: 2, removes: 1 });
 
     let c = 0;
     run(() => {
@@ -268,8 +346,10 @@ module('Integration | Modifier | on', function(hooks) {
       set(this, 'capture', false);
     });
     await settled();
+    assert.counts({ adds: 3, removes: 2 });
 
     await click('button');
+    assert.counts({ adds: 3, removes: 2 });
 
     assert.strictEqual(a, 1);
     assert.strictEqual(b, 1);
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,6 +2,32 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import QUnit from 'qunit';
+import { __counts } from 'ember-on-modifier/modifiers/on';
+
+QUnit.testStart(() => {
+  QUnit.config.current.testEnvironment._startingCounts = __counts();
+});
+
+QUnit.assert.counts = function(
+  expected,
+  msg = `counters have incremented by ${JSON.stringify(expected)}`
+) {
+  const current = __counts();
+
+  this.deepEqual(
+    current,
+    {
+      adds:
+        expected.adds +
+        QUnit.config.current.testEnvironment._startingCounts.adds,
+      removes:
+        expected.removes +
+        QUnit.config.current.testEnvironment._startingCounts.removes
+    },
+    msg
+  );
+};
 
 setApplication(Application.create(config.APP));
 
